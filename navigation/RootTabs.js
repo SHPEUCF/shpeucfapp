2cb39d5e899ab8298e651bca724cbca943fc2ef6
Luis Benavides <luisbenan@gmail.com>
2017-12-10 21:23:09 -0500

    Resolve conflict in RootTabs

--- navigation/RootTabs.js
+++ navigation/RootTabs.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import { TabNavigator } from 'react-navigation';
-=======
-import {TabNavigator} from 'react-native';
 
->>>>>>> 82b477feccaecb458b28f1f28ddf4ecf64497f4d
+import {TabNavigator} from 'react-native';
 
 const RootTabs = TabNavigator({
 
