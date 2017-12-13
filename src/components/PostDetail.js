f3fb32b63c03508893d7f54ea63c816d85eb15bf
Luis Benavides <luisbenan@gmail.com>
2017-12-13 17:58:31 -0500

    Add post detail components using cards

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const PostDetail = (props) => {
   return (
-    <View>
-      <Text>{props.post.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View>
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.post.title}</Text>
+          <Text>{props.post.body}</Text>
+        </View>
+      </CardSection>
+    </Card>
   );
 };
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+}
 
 export default PostDetail;
