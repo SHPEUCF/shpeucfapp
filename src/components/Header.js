ab793201d8bb180b46f8eac7609233504ca53622
Luis Benavides <luisbenan@gmail.com>
2017-12-13 13:17:17 -0500

    create componets folder with initial Header component

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerTitle}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 65,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+
+  },
+  textStyle: {
+    fontSize: 16,
+    fontWeight: '600',
+  }
+};
+
+// Make the component available to other parts of the app
+export default Header;
