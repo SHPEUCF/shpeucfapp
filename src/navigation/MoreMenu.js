55f95fd68054607be0f19ad7a5f2131c7dc79979
grindlock <sergioenriqueperez@gmail.com>
2017-12-17 16:26:34 -0500

    Tab bar is visible on more menu child pages.

--- src/navigation/MoreMenu.js
+++ src/navigation/MoreMenu.js
@@ -23,35 +23,30 @@ const StackNav = StackNavigator({
   JobBoard:{
     screen: JobBoard,
     navigationOptions: {
-      tabBarVisible:false,
       header:null,
     },
   },
   Leaderboard:{
     screen: Leaderboard,
     navigationOptions: {
-      tabBarVisible:false,
       header: null,
     },
   },
   Resources:{
     screen: Resources,
     navigationOptions: {
-      tabBarVisible:false,
       header:null
     },
   },
   CheckIn:{
     screen: CheckIn,
     navigationOptions: {
-      tabBarVisible:false,
       header: null
     },
   },
   About:{
     screen: About,
     navigationOptions: {
-      tabBarVisible:false,
       header:null
     },
   },
