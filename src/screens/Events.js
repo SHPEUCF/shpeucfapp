67a0a5412ef08757eb2c1a16099bfcaf0681bd6f
grindlock <sergioenriqueperez@gmail.com>
2017-12-25 05:58:56 -0500

    Build calendar in events. The back and next button are funtionals. There is a bug, every was in a while it show error: Raw text cannot be used outside of a <Text> tag. Not rendering string:' ' however if you click dismiss everything looks how it should be.

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -1,65 +1,192 @@
 import React, { Component } from 'react';
-import {Image, Text, View, StyleSheet }from 'react-native';
+import { TouchableOpacity, Image, Text, View, StyleSheet }from 'react-native';
 import Moment from 'moment';
 
 import { Header, Card } from '../components/general';
 
+var date = new Date();
+
+let curWeekDay = date.getDay();
+let curDay = date.getDate();
+
+
+var BeginMonth = date.getMonth();
+var BeginYear = date.getFullYear();
+
+
+let fullMonthName = ["January  ","February  ",
+"March  ","April  ","May  ","June  ",
+"July  ","August  ","September  ","October  "
+,"November  ","December  "];
+let shortWeekDayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+
+
 
 class Events extends Component {
 
-//new Date(2017, 11 + 1, 0).getDate() returns the amount of days in the month
+state = { month:BeginMonth, year:BeginYear };
 
-  render() {
-    const curDate =new Date();
-    const curMonth = new Date().getMonth();
-    const curWeekDay = new Date().getDay();
-    const curDay = new Date().getDate();
-    const curYear = new Date().getFullYear();
-    const monthDays = new Date(curYear, curMonth + 1, 0).getDate()
+prevMonth = () => {
 
+     if (this.state.month > 0) {
+         this.setState({month: (this.state.month- 1)});
+         this.setState({year: (this.state.year)});
+     } else {
+         this.setState({month: 11});
+         this.setState({year: (this.state.year - 1)});
+     }
+ };
 
-    return (
-      <View style={{flex:1, }}>
-        <Header headerTitle={'Events'} />
-        <View style={styles.container}>
-          {/*<Image style={{margin:10, height:100, width:100, backgroundColor:'orange'}} source={{uri:'https://reactjs.org/logo-og.png'}}/>
-          <Card style={{fle:1,height:20, width:40, backgroundColor:'green'}}/>*/}
-          <View style={{flex:1, flexDirection:'row', marginLeft:5, marginRight:5}}>
-
-            {shortWeekDayName.map((item, key)=>(
-              <View key={key} style={{flex:1, alignItems:'center',
-                justifyContent:'center',height:50,
-                backgroundColor:'grey',borderWidth:3,
-                borderColor:'white'}}>
-                <Text key={key} style={{fontWeight:'900', color:'white'}}>
-                  {item}
-                </Text>
-              </View>))}
-
-
-          </View>
-        </View>
-      </View>
-    )
+nextMonth = () => {
+  if (this.state.month < 11) {
+      this.setState({month: (this.state.month + 1)});
+      this.setState({year: (this.state.year)});
+  } else {
+      this.setState({month: 0});
+      this.setState({year: (this.state.year + 1)});
   }
 }
+  render() {
+
+    return (
+<View style={{flex:1, }}>
+
+  <Header headerTitle={'Events'}/>
+    <View style={styles.container}>
+
+      <View style={{flex:0,flexDirection:'row', alignItems:'center',}}>
+        <TouchableOpacity style={{flex:1}}
+           onPress={ this.prevMonth.bind(this)} >
+        <Text style={{textAlign:'left', fontSize:20}}>{"< Back"}</Text>
+      </TouchableOpacity>
+    <Text style={{textAlign:'center', fontSize:24}}>{fullMonthName[this.state.month].concat(this.state.year)}</Text>
+      <TouchableOpacity style={{flex:1}}
+        onPress = { this.nextMonth.bind(this)} >
+        <Text style={{textAlign:'right', fontSize:20}}>{"Next >"}</Text>
+      </TouchableOpacity>
+    </View>
+
+      <View style={{flex:0,flexDirection:'row', }}>
+              { this.renderWeekDays()}
+      </View>
+
+              { this.renderCalendarDays() }
+    </View>
+
+</View>
+    )
+  }
+
+  renderWeekDays(){
+    return shortWeekDayName.map((item, key)=>{
+      return(
+        <View key={key} style={{flex:1, alignItems:'center',
+          justifyContent:'center',height:50,
+          backgroundColor:'grey',borderWidth:3,
+          borderColor:'white'}}>
+          <Text key={key}
+            style={{fontWeight:'900',
+               color:'white'}}>
+            {item}
+          </Text>
+        </View>
+      );
+    });
+  }
+
+  renderDays(week_days) {
+      return week_days.map((day, key) => {
+          return (
+
+            <View
+              key={key}
+              style={{flex:1,
+                alignItems:'center',
+                justifyContent:'center',
+
+                backgroundColor:'lightgrey',
+
+                borderWidth:3,
+                borderColor:'white'}}>
+              <Text key={key} style={{
+                  fontSize:18,
+                  fontWeight:'100'
+                }}>{day}</Text>
+              <Card> </Card>
+            </View>
+
+          );
+      });
+  }
+
+  renderCalendarDays(){
+    var monthDays = new Date(this.state.year, this.state.month + 1, 0).getDate()
+    var startMonth = new Date(this.state.year, this.state.month, 1).getDay();
+    var endMonth = new Date(this.state.year, this.state.month,monthDays).getDay();
 
 
+    var test2 = [];
+    var count = 0;
+    var oneWeek = [];
+    var weeks = [];
+    var totalD = 0;
+    var d = 1;
+    {/*if((startMonth * 7) - monthDays <= 7){*/}
+      totalD = startMonth  + monthDays + ( 7 - endMonth);
 
-const fullMonthName = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const shortWeekDayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+    {/*creates the days array.*/}
+    for(var i = 0; i <= totalD ; i++){
+      if(i < startMonth){
+        test2[i] = " ";
+      }
+      else if(d <= monthDays){
+        test2[i] = d;
+        d++;
+      }
+      else{
+        test2[i] = " ";
+      }
+
+    }
+    {/*creates an array with seven days array.*/}
+     test2.forEach((item) => {
+       count += 1;
+       oneWeek.push(item);
+       if(count == 7){
+         weeks.push(oneWeek);
+         oneWeek = [];
+         count = 0;
+       }
+     });
+
+     return weeks.map((item, key) => {
+       return(
+         <View key={key} style={{flex:0,
+             flexDirection:'row',
+             alignItems:'center',
+             justifyContent:'center'}}>
+           { this.renderDays(item) }
+         </View>
+       );
+     });
+
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
+    alignItems:'center',
+    justifyContent:'flex-start',
     marginTop:5,
-    justifyContent:'space-between',
-    alignItems:'flex-start',
+    marginLeft:5,
+    marginRight:5
   },
   text:{
     textAlign:'center',
 
-  }
+  },
+
 });
 
 export { Events };
