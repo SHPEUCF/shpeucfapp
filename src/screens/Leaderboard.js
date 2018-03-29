f0b0b599567b0eef3353349afdeb0b4c5fa65861
I-Lopez <igyalopez9936@gmail.com>
2018-03-29 18:02:38 -0400

    React Native progressbar flatlist leaderboards

--- src/screens/Leaderboard.js
+++ src/screens/Leaderboard.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-import {FlatList, Platform, Text, View, StyleSheet, TouchableOpacity, Dimensions, ProgressViewIOS, ProgressBar }from 'react-native';
-=======
->>>>>>> 840c6af6f637ffc6fa79ea9c22581e9f62673be1
 import { fetchMembersPoints } from '../actions';
 import _ from 'lodash';
+import * as Progress from 'react-native-progress';
 import {
   FlatList,
   Text,
   View,
   StyleSheet,
   TouchableOpacity,
-  Dimensions,
-  ProgressViewIOS } from 'react-native';
+  Dimensions } from 'react-native';
 
 const dimension = Dimensions.get('window');
 const iteratees = ['points','lastName','firstName'];
 const order = ['desc','asc','asc'];
 
 class Leaderboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      animated: false,
+      indeterminate: true,
+      progress: .5
+    };
+  }
+
   componentWillMount() {
     this.props.fetchMembersPoints();
   }
@@ -29,57 +35,48 @@ class Leaderboard extends Component {
   render() {
     const {
       containerStyle,
-      contentContainerStyle } = styles;
+      contentContainerStyle,
+      progress } = styles;
     const sortedMembers = _.orderBy(this.props.membersPoints, iteratees, order);
 
-
-    return Platform.OS === 'ios' ?(
+    return (
       <FlatList
           data={sortedMembers}
           extraData={this.state}
           keyExtractor={this._keyExtractor}
           renderItem={({item, separators}) => (
           <View style={contentContainerStyle}>
+            <View style={containerStyle}>
               <Text>{`${item.firstName} ${item.lastName}`}</Text>
               <Text>Points:{item.points}</Text>
-              <ProgressViewIOS
-                progress = {item.points / sortedMembers[0].points}
-                disabled
-                thumbTintColor='transparent'/>
+              <Progress.Bar
+                style={styles.progress}
+                progress={.03}
+                indeterminate={this.state.indeterminate}
+                width={dimension.width * .9}
+              />
             </View>
-          )}
-      />
-  ) :
-  (
-    <FlatList
-        data={sortedMembers}
-        extraData={this.state}
-        renderItem={({item, separators}) => (
-        <View style={contentContainerStyle}>
-            <Text>{`${item.firstName} ${item.lastName} `}</Text>
-            <Text>Points:{item.points}</Text>
-            <ProgressViewIOS
-              progress = {item.points / sortedMembers[0].points}
-              disabled
-              thumbTintColor='transparent'/>
           </View>
         )}
-    />
-)
+      />
+    )
   }
 }
 
 const styles = StyleSheet.create({
   containerStyle: {
     flex: 1,
-    backgroundColor: '#FFF',
-    paddingTop: 20
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    paddingVertical: 30,
   },
   contentContainerStyle: {
-    paddingLeft: dimension.width * .05,
-    paddingRight: dimension.width * .05,
     margin: 1,
     backgroundColor: '#abc',
+  },
+  progress: {
+    justifyContent: 'center',
   }
 });
 
