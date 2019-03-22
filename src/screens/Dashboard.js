aefe34612a9fc1e3f77f9900cac929462904e9a4
AgentIMR <idel_martinez@knights.ucf.edu>
2019-03-22 19:35:54 -0400

    Modified styling of leaderboard, events and the remaining containers. Add buttons to the Slack page and the website. Committees was added as a placeholder.

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,54 +1,39 @@
 import firebase from 'firebase';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ScrollView, FlatList, Dimensions, TouchableOpacity } from 'react-native';
-import { loadUser, pageLoad, fetchMembersPoints, fetchMemberProfile, fetchEvents,
-	getPrivilege,
-	typeChanged,
-	nameChanged,
-	descriptionChanged,
-	dateChanged,
-	timeChanged,
-	locationChanged,
-	epointsChanged,
-	eventIDChanged,
-	goToCreateEvent,
-	goToViewEvent } from '../actions';
+import { Text, View, StyleSheet, ScrollView, FlatList, Dimensions, TouchableOpacity, WebView, Linking } from 'react-native';
 import * as Progress from 'react-native-progress';
 import _ from 'lodash';
 import { Actions } from 'react-native-router-flux';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import {
+	loadUser,
+	pageLoad,
+	fetchMembersPoints,
+	fetchMemberProfile,
+	fetchEvents,
+	getPrivilege } from '../actions';
 
 const dimension = Dimensions.get('window');
 const iteratees = ['points','lastName','firstName'];
 const order = ['desc','asc','asc'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June',
-'July', 'August', 'September', 'October', 'November', 'December'];
 
 class Dashboard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			date: ''
-		};
-	}
-
-	componentDidMount() {
-	 }
-
 	componentWillMount() {
 		this.props.pageLoad();
 		this.props.fetchMembersPoints();
 		this.props.fetchEvents();
 		this.props.loadUser();
-
-	 }
+	}
   
 	callUser(id){
 		this.props.pageLoad();
 		this.props.fetchMemberProfile(id);
-	 }
+	}
 
 	greeting() {
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+							 'August', 'September', 'October', 'November', 'December'];
 		const date = new Date();
 		let time = date.getHours();
 		let day = date.getDate();
@@ -68,60 +53,50 @@ class Dashboard extends Component {
 		let fields = [];
 
 		for (key in events) {
-			fields.push(`${events[key].name}  ${events[key].date}`)			
+			fields.push([`${events[key].name}`, `${events[key].date}`, `${events[key].description}`]);
 		}
 
-		if(fields.length > 0)
+		if (fields.length > 0)
 			field = fields.pop();
-		else{
+		else
 			field = "No events coming soon"
+		
+		let eventName = field[0], eventDate = field[1], eventDesc = field[2];
+		if (eventDesc !== undefined && eventDesc.length > 75) {
+			eventDesc = eventDesc.slice(0, 75);
+			eventDesc += '...'
 		}
 
-		return <Text>{field}</Text>
-	
-
-		
+		return(
+			<View style={{alignItems:'center'}}>
+				<Text style={{fontStyle: 'italic', fontSize: 16}}>{eventName}</Text>
+				<Text style={{paddingBottom: '5%'}}>{eventDate}</Text>
+				<Text style={{marginLeft: '10%', marginRight: '10%'}}>{eventDesc}</Text>
+			</View>
+		)
 	}
 
-	viewEvent(item) {
-	this.props.nameChanged(item.name);
-   	this.props.descriptionChanged(item.description);
-   	this.props.dateChanged(item.date);
-   	this.props.timeChanged(item.time);
-   	this.props.locationChanged(item.location);
-   	this.props.epointsChanged(item.points);
-   	this.props.eventIDChanged(item.eventID);
-   	this.props.goToViewEvent();
-	}
-	isDefined(obj){
+	isDefined(obj) {
 		return !(obj === undefined || obj == null || obj.length < 2)
 	}
 
-	renderEvent(item) {
-		return (
-		  <TouchableOpacity onPress={this.props.bind.viewEvent(this, item)}>
-				<View style={styles.item}>
-				  <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
-				  <Text>Date: {item.date}</Text>
-				  <Text>Time: {item.time}</Text>
-				  <Text>Location: {item.location}</Text>
-				  <Text style={styles.description}>Description: {item.description}</Text>
-				  <Text>Points: {item.points}</Text>
-			 </View>
-		  </TouchableOpacity>
-		);
-	 }
 	renderComponent(item, sortedMembers) {
 		const {
-		  containerStyle,
-		  contentContainerStyle,
-		  progress
+			contentContainerStyle,
+			progress,
+			index,
+			indexText
 		} = styles;
+
 		return (
 			<View style={contentContainerStyle}>
-				<Text style={{alignSelf: 'center', fontWeight: "700", margin: 3}}>{item.index}</Text>
-				<View style={containerStyle}>
+				<View style={index}>
+					<Text style={indexText}>{item.index}</Text>
+				</View>
+				
+				<View>
 					<Text>{item.firstName} {item.lastName === undefined ? '' : item.lastName}</Text>
+					<Text>Points: {item.points}</Text>
 					<Progress.Bar
 						style={progress}
 						progress={item.points / Math.max(sortedMembers[0].points, 1)}
@@ -131,9 +106,10 @@ class Dashboard extends Component {
 				</View>
 			</View>
 		)		
-	 }
+	}
 
 	_keyExtractor = (item, index) => index;
+
    render() {
       const {
 			page,
@@ -142,15 +118,19 @@ class Dashboard extends Component {
 			mainContentStyle,
 			greetingContainerStyle,
 			firstContainerStyle,
-			leaderboardTitle } = styles;
-		  const { currentUser } = firebase.auth();
+			title,
+			touchLeaderboard,
+			eventsContainer
+		} = styles;
+		
+		const { currentUser } = firebase.auth();
 
 		let sortedMembers = _.orderBy(this.props.membersPoints, iteratees, order);
-		var currentMember
+		var currentMember;
 		sortedMembers.forEach((x, index) => {
-			x.index = (x.points !== 0) ? index + 1 : sortedMembers.length
-			if(x.id === currentUser.uid) {
-				currentMember =  x
+			x.index = (x.points !== 0) ? index + 1 : sortedMembers.length;
+			if (x.id === currentUser.uid) {
+				currentMember =  x;
 			};
 		});
 		sortedMembers.splice(2);
@@ -162,35 +142,43 @@ class Dashboard extends Component {
 				<View style={tabBar}>
 					<Text style={tabBarText}>Dashboard</Text>
 				</View>
-				<View style={mainContentStyle}>
-					<View style={greetingContainerStyle}>
-						{this.greeting()}
-					</View>
-					<View style={firstContainerStyle}>
-						<TouchableOpacity style={{flex: 1, flexDirection:'column', alignItems:'center', paddingRight: 10}} onPress={() => Actions.Leaderboard()}>
-								<Text style={leaderboardTitle}>Leaderboard</Text>
-								<FlatList
-									data={sortedMembers}
-									extraData={this.state}
-									keyExtractor={this._keyExtractor}
-									renderItem={({item}) => (
-									this.renderComponent(item, sortedMembers))}
-								/>
-						</TouchableOpacity>
-						<View style={{flex: 1, flexDirection:'column', alignItems:'center'}}>
-							<Text style={{fontSize: 20, paddingBottom: 10}}>Upcoming Events</Text>
-							{this.getFormattedEventList()}
+				<ScrollView>
+					<View style={mainContentStyle}>
+						<View style={greetingContainerStyle}>
+							{this.greeting()}
 						</View>
+						<View style={firstContainerStyle}>
+							<View style={{flexDirection: 'row'}}>
+								<TouchableOpacity style={touchLeaderboard} onPress={() => Actions.Leaderboard()}>
+									<Text style={title}>Leaderboard</Text>
+									<FlatList
+										data={sortedMembers}
+										extraData={this.state}
+										keyExtractor={this._keyExtractor}
+										renderItem={({item}) => (this.renderComponent(item, sortedMembers))}
+									/>
+								</TouchableOpacity>
+								<TouchableOpacity style={eventsContainer} onPress={() => alert('Coming soon!')}>
+									<Text style={title}>Upcoming Events</Text>
+									<View style={{alignSelf: 'center'}}>
+										{this.getFormattedEventList()}
+									</View>
+								</TouchableOpacity>
+							</View>
+						</View>
+						<View style={firstContainerStyle}>
+							<Text style={title}>Committees</Text>
+							<Text>Coming soon!</Text>
+						</View>
+						<View style={firstContainerStyle}>
+							<Text style={title}>Join our Slack!</Text>
+							<FontAwesomeIcon name="slack" size={30} onPress={() => Linking.openURL('https://shpeucf2018-2019.slack.com/')}/>
+						</View>
+						<TouchableOpacity style={firstContainerStyle} onPress={() => Linking.openURL('https://www.shpeucf.com/')}>
+							<Text style={[title, {paddingBottom: 0}]}>Visit our website!</Text>
+						</TouchableOpacity>
 					</View>
-					<View style={{paddingLeft: 10}}>
-						<Text>
-							To be added:{'\n'}
-							- Committees{'\n'}
-							- Join our Slack!{'\n'}
-							- Visit our website!{'\n'}
-						</Text>
-					</View>
-				</View>
+				</ScrollView>
          </View>
       );
 	}
@@ -204,7 +192,7 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		justifyContent: 'center',
 		alignItems:'center',
-		height: dimension.height * .1
+		height: dimension.height * 0.1
 	},
 	tabBarText: {
 		color: '#000',
@@ -216,53 +204,81 @@ const styles = StyleSheet.create({
 		padding: '3%'
 	},
 	contentContainerStyle: {
-		flexDirection: 'row'
-	},
-	buttonsContainerStyle: {
-		marginRight: 10,
-		marginLeft: 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingBottom: '5%'
 	},
 	firstContainerStyle: {
 		alignItems: 'center',
 		justifyContent: 'center',
-		flexDirection: 'row',
 		backgroundColor: '#fff',
 		borderRadius: 10,
-		padding: '3%',
-		margin: '3%'
+		paddingTop: '2%',
+		paddingBottom: '2%',
+		paddingLeft: '3%',
+		paddingRight: '3%',
+		marginTop: '2%',
+		marginLeft: '3%',
+		marginRight: '3%',
+		marginBottom: '2%',
+		elevation: 1
 	 },
 	mainContentStyle: {
 		color: '#000'
 	},
 	progress: {
-		width: dimension.width * .36,
+		width: dimension.width * .32,
 		justifyContent: 'center',
 	},
-	leaderboardTitle: {
-		fontSize: 16,
-		fontWeight: '700'
+	title: {
+		fontSize: 18,
+		fontWeight: '500',
+		paddingBottom: '5%'
+	},
+	touchLeaderboard: {
+		flex: 1,
+		flexDirection:'column',
+		alignItems:'center',
+		paddingRight: '3%'
+	},
+	index: {
+		borderColor: 'black',
+		borderStyle: 'solid',
+		borderWidth: 1.5,
+		borderRadius: 11,
+		marginRight: '4%',
+		justifyContent:'center',
+		height: 22,
+		width: 22,
+		elevation: 1
+	},
+	indexText: {
+		alignSelf: 'center',
+		fontWeight: "700",
+		fontSize: 11
+	},
+	eventsContainer: {
+		flex: 1,
+		flexDirection:'column',
+		alignItems:'center'
 	}
 });
 
 const mapStateToProps = ({ auth, general, members, events }) => {
-	const { firstName, id} = auth;
+	const { firstName, id } = auth;
 	const { loading } = general;
 	const { membersPoints } = members;
 	const { eventList } = events;
 	return { firstName, id, loading, membersPoints, eventList };
 };
  
- const mapDispatchToProps = {	loadUser, pageLoad, fetchMembersPoints, fetchMemberProfile, fetchEvents,
-	getPrivilege,
-	typeChanged,
-	nameChanged,
-	descriptionChanged,
-	dateChanged,
-	timeChanged,
-	locationChanged,
-	epointsChanged,
-	eventIDChanged,
-	goToCreateEvent,
-	goToViewEvent };
+ const mapDispatchToProps = {	
+	loadUser,
+	pageLoad,
+	fetchMembersPoints,
+	fetchMemberProfile,
+	fetchEvents,
+	getPrivilege
+};
  
  export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
