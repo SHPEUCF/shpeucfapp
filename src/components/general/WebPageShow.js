d280ca864d32e8b79e1113cc004015f3c64a2a34
joseiciano <icianojose12345@aol.com>
2019-11-21 14:56:01 -0500

    about fixed

--- src/components/general/WebPageShow.js
+++ src/components/general/WebPageShow.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import { WebView, View } from 'react-native';
-import { Spinner } from './Spinner';
-import { NavBar }  from './NavBar';
-import { Actions } from 'react-native-router-flux';
+import React, { Component } from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
+import { Spinner } from "./Spinner";
+import { NavBar } from "./NavBar";
+import { Actions } from "react-native-router-flux";
 
 class WebPageShow extends Component {
   render() {
     const { uri } = this.props;
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <NavBar back onBack={() => Actions.pop()} />
         <WebView
           source={{ uri }}
-          renderLoading={() => <Spinner /> }
+          renderLoading={() => <Spinner />}
           startInLoadingState
         />
       </View>
--- src/screens/About.js
+++ src/screens/About.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react';
-import {Text, View, StyleSheet }from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import { List, ListItem } from 'react-native-elements';
-import { NavBar } from '../components/general';
+import React, { Component } from "react";
+import { View } from "react-native";
+import { Actions } from "react-native-router-flux";
+import { ListItem } from "react-native-elements";
+import { NavBar } from "../components/general";
 
-  const menuItems = [
-    // {
-    //   title: 'E-Board',
-    //   icon: 'people',
-    //   screen: 'EBoard'
-    // },
-    {
-      title: 'Privacy Policy',
-      icon: 'insert-drive-file',
-      screen: 'WebPageShow',
-      uri:"http://www.shpeucf.com/privacy-policy/"
-    },
-    {
-      title: 'Version',
-      icon: 'beenhere',
-      screen: 'Version',
-      content: "Current version: v1.0-alpha",
-      footer: 'Check our GitHub Page - SHPEUCF'
-    }
-    ];
+const menuItems = [
+  {
+    title: "E-Board",
+    icon: "people",
+    screen: "EBoard"
+  },
+  {
+    title: "Privacy Policy",
+    icon: "insert-drive-file",
+    screen: "WebPageShow",
+    uri: "http://www.shpeucf.com/privacy-policy/"
+  },
+  {
+    title: "Version",
+    icon: "beenhere",
+    screen: "Version",
+    content: "Current version: v1.0-alpha",
+    footer: "Check our GitHub Page - SHPEUCF"
+  }
+];
 
 class About extends Component {
-
   render() {
     return (
-      <View style={{flex: 1, backgroundColor:'#2C3239'}}>
+      <View style={{ flex: 1, backgroundColor: "#2C3239" }}>
         <NavBar title="About" back onBack={() => Actions.pop()} />
-        <List>
-        <View style={{backgroundColor:'#2C3239'}}>
-          {
-            menuItems.map((menuItem, i) => (
-              <ListItem
-                key={i}
-                title={menuItem.title}
-                titleStyle={{ color: 'white'}}
-                leftIcon={{name: menuItem.icon}}
-                onPress={() => Actions[menuItem.screen]({ title: menuItem.title,
-                                                          uri: menuItem.uri,
-                                                          content: menuItem.content,
-                                                          footer: menuItem.footer })}
-              />
-            ))
-          }
-          </View>
-        </List>
+        <View style={{ backgroundColor: "#2C3239" }}>
+          {menuItems.map((menuItem, i) => (
+            <ListItem
+              key={i}
+              title={menuItem.title}
+              titleStyle={{ color: "black" }}
+              leftIcon={{ name: menuItem.icon }}
+              onPress={() =>
+                Actions[menuItem.screen]({
+                  title: menuItem.title,
+                  uri: menuItem.uri,
+                  content: menuItem.content,
+                  footer: menuItem.footer
+                })
+              }
+            />
+          ))}
+        </View>
       </View>
     );
-  };
+  }
 }
 
 export { About };
