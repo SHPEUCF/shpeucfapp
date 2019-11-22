c9699e93c6c7a879e0740cfbc962ce5e93983366
esteban737 <perdomo.steven.77@knights.ucf.edu>
2019-11-22 00:43:10 -0500

    Integrated crop profile picture functionality into QR code branch and included permissions for both platforms

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -8,7 +8,7 @@ import { loadUser, logoutUser, goToEditProfileForm, pageLoad, pictureChanged} fr
 import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import { Avatar, Divider } from 'react-native-elements';
 import ImagePicker from 'react-native-image-crop-picker';
-import RNFetchBlob from 'react-native-fetch-blob';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const dimension = Dimensions.get('window');
 class Profile extends Component {
@@ -105,7 +105,8 @@ class Profile extends Component {
       includeBase64: true,
       compressImageQuality: 0.8,
       mediaType: 'photo',
-      cropping: true
+      cropping: true,
+      cropperCircleOverlay: true
     }).then(image => {
 
       const imagePath = image.path
