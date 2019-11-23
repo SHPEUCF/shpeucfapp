a90cebf70fd6800c9cdd53230c4243cc86733b60
joseiciano <icianojose12345@aol.com>
2019-11-22 19:48:29 -0500

    fixed about

--- src/components/general/WebPageShow.js
+++ src/components/general/WebPageShow.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
-import { WebView, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
 import { Spinner } from "./Spinner";
 import { NavBar } from "./NavBar";
 import { Actions } from "react-native-router-flux";
 
-class WebPageShow extends Component {
-  render() {
-    const { uri } = this.props;
-    return (
-      <View style={{ flex: 1 }}>
-        <NavBar back onBack={() => Actions.pop()} />
-        <WebView source={{ uri }} renderLoading={() => <Spinner />} startInLoadingState />
-      </View>
-    );
-  }
-}
+const WebPageShow = props => {
+  const { uri } = props;
+  return (
+    <View style={{ flex: 1 }}>
+      <NavBar back onBack={() => Actions.pop()} />
+      <WebView source={{ uri }} renderLoading={() => <Spinner />} startInLoadingState />
+    </View>
+  );
+};
 
 export { WebPageShow };
