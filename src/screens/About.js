a3ad6a9f4d55f29acd04d750f4c9a07f7c18635c
esteban737 <perdomo.steven.77@knights.ucf.edu>
2020-01-18 13:25:12 -0500

    Restyled the about page and fixed webpageview

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet }from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, FlatList, Dimensions }from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { List, ListItem } from 'react-native-elements';
 import { NavBar } from '../components/general';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const dimension = Dimensions.get('window');
 
   const menuItems = [
     // {
@@ -29,29 +31,41 @@ class About extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, backgroundColor:'#2C3239'}}>
-        <NavBar title="About" back onBack={() => Actions.pop()} />
-        <List>
-        <View style={{backgroundColor:'#2C3239'}}>
-          {
-            menuItems.map((menuItem, i) => (
-              <ListItem
-                key={i}
-                title={menuItem.title}
-                titleStyle={{ color: 'white'}}
-                leftIcon={{name: menuItem.icon}}
-                onPress={() => Actions[menuItem.screen]({ title: menuItem.title,
-                                                          uri: menuItem.uri,
-                                                          content: menuItem.content,
-                                                          footer: menuItem.footer })}
-              />
-            ))
-          }
-          </View>
-        </List>
-      </View>
+      <SafeAreaView style={{backgroundColor: '#0c0b0b', flex: 1}}>
+        <NavBar title="About" back onBack={() => Actions.pop()} ></NavBar>
+        <View style = {{backgroundColor: "black"}}>
+        <FlatList
+          removeClippedSubviews={false}
+          extraData={this.props}
+          keyExtractor = {this.keyExtractor}
+          data = {menuItems}
+          renderItem={this.renderItem}
+        />
+        </View>
+      </SafeAreaView>
     );
-  };
+  }
+
+  keyExtractor = (item, index) => index
+
+  renderItem  = ({item}) => {
+      return(
+        <View style = {{backgroundColor: "black"}}>
+          <ListItem
+              containerStyle={{ backgroundColor: 'black', borderBottomColor: 'black'}}
+              removeClippedSubviews={false}
+              title={item.title}
+              titleStyle={{ color: 'white'}}
+              leftIcon={{name: item.icon , color: 'white'}}
+              rightIcon={<Ionicons name="ios-arrow-dropright" size={dimension.height * .025} style={{color: '#FECB00'}}/>}
+              onPress={() => Actions[item.screen]({ title: item.title,
+              uri: item.uri,
+              content: item.content,
+              footer: item.footer })}
+              />
+        </View>
+      )
+  }
 }
 
 export { About };
