c8afab971a1ef14cb30502f91f2694fc9cc8b200
mechaturtles <bryce.christian@gmail.com>
2020-05-15 00:36:02 -0500

    More changes per requests

--- src/components/general/DatePicker.js
+++ src/components/general/DatePicker.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
-import { Input } from "./Input";
-import { PickerInput } from "./PickerInput";
+import { PickerInput } from "./";
 import { prepend0 } from "../../utils/events";
 
 class DatePicker extends Component {
@@ -11,13 +10,13 @@ class DatePicker extends Component {
 
 		let date = [];
 		if (this.props.value) date = this.props.value.split("-");
-		console.log(date);
+
 		this.state = {
 			month: date.length === 3 ? date[1] : "",
 			day: date.length === 3 ? date[2] : "",
 			year: date.length === 3 ? date[0] : "",
 			monthArray: Array.from({ length: 12 }, (v, k) => k + 1),
-			yearArray: Array.from({ length: 20 }, (v, k) => new Date().getFullYear() + k) // We only need about 20 years ahead... right?
+			yearArray: Array.from({ length: 20 }, (v, k) => new Date().getFullYear() + k)
 		};
 	}
 
@@ -33,44 +32,42 @@ class DatePicker extends Component {
 		const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 		const monthInt = parseInt(month);
 
-		// If the state's day is more than the max days in a month, set the day to the max day
 		const setMaxDay = (maxDay) => { if (day > maxDay) this.setState({ day: maxDay }); };
 
 		if (monthInt == 2 && this.isLeapYear(year)) {
 			setMaxDay(29);
+
 			return Array.from({ length: 29 }, (v, k) => k + 1);
 		}
 		setMaxDay(daysInMonth[monthInt - 1]);
+
 		return Array.from({ length: daysInMonth[monthInt - 1] }, (v, k) => k + 1);
 	}
 
-	/**
-	 * Returns true if the input year is a leap year. Must be an int.
-	 * @param {Number} year Any year such as 2020 or 101
-	*/
-	isLeapYear(year) { return (year % 4 === 0) && (year % 100 != 0) || (year % 400 == 0) }
+	/** Returns true if the input year is a leap year. Must be an int. */
+	isLeapYear = (year) => (year % 4 === 0) && (year % 100 != 0) || (year % 400 == 0)
 
 	/** Sets the date by calling the onSelect function in the Form component. */
-	setDate(item) {
-		const { month, day, year } = item;
-		this.props.onSelect(`${year}-${month}-${day}`);
-	}
+	setDate = ({ month, day, year }) => this.props.onSelect(`${year}-${month}-${day}`)
 
 	/**
 	 * Calls the setDate function and sets the month/day/year based on a given value and type.
+	 * Month and day will be prepended with a single 0.
 	 * @param {Number} item Any number.
-	 * @param {String} type A string that is "month", "day", "year".
+	 * @param {String} type String that describes which value is being changed. Must be "month", "day", or "year".
 	*/
-	setMDY(item, type) {
+	changeStateOfType(item, type) {
 		const { month, day, year } = this.state;
-		// Month and Day is prepended with a 0.
-		// Do note that the day January 1, 2000 will show up as 01/01/2000 instead of 1/1/2000.
+
 		switch (type) {
-			case "month": this.setState({ month: prepend0(item) });
+			case "month":
+				this.setState({ month: prepend0(item) });
 				break;
-			case "day": this.setState({ day: prepend0(item) });
+			case "day":
+				this.setState({ day: prepend0(item) });
 				break;
-			case "year": this.setState({ year: item });
+			case "year":
+				this.setState({ year: item });
 				break;
 		}
 
@@ -95,7 +92,7 @@ class DatePicker extends Component {
 						iconSize = { iconSize }
 						iconColor = 'black'
 						value = { month }
-						onSelect = { (text) => this.setMDY(text, "month") }
+						onSelect = { (text) => this.changeStateOfType(text, "month") }
 						placeholder = { "MM" }
 					/>
 				</View>
@@ -109,7 +106,7 @@ class DatePicker extends Component {
 						iconSize = { iconSize }
 						iconColor = 'black'
 						value = { day }
-						onSelect = { (text) => this.setMDY(text, "day") }
+						onSelect = { (text) => this.changeStateOfType(text, "day") }
 						placeholder = { "DD" }
 					/>
 				</View>
@@ -123,7 +120,7 @@ class DatePicker extends Component {
 						iconColor = 'black'
 						dropDownArrowStyle = { dropDownArrowStyle }
 						value = { year }
-						onSelect = { (text) => this.setMDY(text, "year") }
+						onSelect = { (text) => this.changeStateOfType(text, "year") }
 						placeholder = { "YYYY" }
 					/>
 				</View>
