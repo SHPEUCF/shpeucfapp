ea03bd9cad30631bd1bdabeb3c3e802945cd3f16
idelmr <idel_martinez@knights.ucf.edu>
2020-12-20 10:44:16 -0500

    Add documentation

--- src/components/general/ProgressBar.js
+++ src/components/general/ProgressBar.js
@@ -1,5 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React, { useRef, useEffect } from "react";
-import { Animated, View } from "react-native";
+import { Animated, View, ViewStyle } from "react-native";
+
+/**
+ * @desc Progress Bar component for leaderboard.
+ *
+ * @typedef {Object} ProgressBarProps
+ * @prop {Number}          progress  Progress indication on bar.
+ * @prop {Number | String} width     Progress bar width.
+ * @prop {Number | String} height    Progress bar height.
+ * @prop {String}          color     Color of progress indicator.
+ * @prop {ViewStyle}       style
+ *
+ * @param {...ProgressBarProps}
+ */
 
 export const ProgressBar = ({ progress, width, height, color, style }) => {
 	const progressWidth = useRef(new Animated.Value(progress * width)).current;
@@ -8,9 +22,11 @@ export const ProgressBar = ({ progress, width, height, color, style }) => {
 		Animated.timing(progressWidth, { toValue: progress * width, duration: 1000 }).start();
 	}, [progress]);
 
+	let progressBorder = (progress === 1) ? styles.bar : styles.progress;
+
 	return (
-		<View style = { [style, styles.bar, { width, height, elevation: 1 }] }>
-			<Animated.View style = {{ backgroundColor: color, height, width: progressWidth }} />
+		<View style = { [style, styles.bar, { width, height }] }>
+			<Animated.View style = { [progressBorder, { backgroundColor: color, height, width: progressWidth }] } />
 		</View>
 	);
 };
@@ -18,5 +34,9 @@ export const ProgressBar = ({ progress, width, height, color, style }) => {
 const styles = {
 	bar: {
 		borderRadius: 5
+	},
+	progress: {
+		borderBottomLeftRadius: 5,
+		borderTopLeftRadius: 5
 	}
 };
