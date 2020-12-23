8c8e822f3c011ffe1411b8d2e0922c0bcfa4e76d
idelmr <idel_martinez@knights.ucf.edu>
2020-12-23 16:38:11 -0500

    Add optional edit button

--- src/components/general/Avatar.js
+++ src/components/general/Avatar.js
@@ -1,25 +1,28 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import FastImage from "react-native-fast-image";
-import { TouchableOpacity, Text, Dimensions } from "react-native";
+import { TouchableOpacity, Text, Dimensions, TextStyle } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const { height } = Dimensions.get("screen");
 
 /**
  * @description Avatar component to be used with images or text.
  *
- * @typedef {Object} Props
- * @prop {String}                                    title       Text to display instead of an image.
- * @prop {Object}                                    titleStyle  Style of text style.
+ * @typedef {Object} AvatarProps
  * @prop {'square'|'circle'|'rounded'}               shape       Shape of Avatar.
  * @prop {Number|'small'|'medium'|'large'|'xlarge'}  size        Size of Avatar.
- * @prop {{uri: String}}                             source      Image source.
+ * @prop {String}                                    source      Image source.
+ * @prop {String}                                    title       Text to display instead of an image.
+ * @prop {TextStyle}                                 titleStyle  Style of text style.
+ * @prop {Boolean}                                   showEdit    Show edit button for Avatar.
  * @prop {Function}                                  onPress     Called when the Avatar is pressed.
  * @prop {Object}                                    style       Additional style for Avatar.
  *
- * @param {...Props}
+ * @param {...AvatarProps}
  */
 
-export const Avatar = ({ title, titleStyle, shape = "circle", size = "medium", source, onPress, style }) => {
+export const Avatar = ({ shape = "circle", size = "medium", source, title, titleStyle, onPress, showEdit, style }) => {
 	let borderRadius;
 
 	switch (size) {
@@ -44,17 +47,25 @@ export const Avatar = ({ title, titleStyle, shape = "circle", size = "medium", s
 			borderRadius = 15;
 	}
 
-	const { center } = styles;
+	const { center, editButton } = styles;
+	let avatarSize = { height: size, width: size, borderRadius };
 
 	return (
 		<TouchableOpacity
-			activeOpacity = { (!onPress) ? 1 : 0.2 }
-			onPress = { () => onPress && onPress() }
-			style = { [style, center, { height: size, width: size, borderRadius }] }
+			activeOpacity = { (showEdit || !onPress) ? 1 : 0.2 }
+			onPress = { () => !showEdit && onPress && onPress() }
+			style = { [center, avatarSize, style] }
 		>
-			{ source
-				? <FastImage style = {{ height: size, width: size, borderRadius }} source = { source } />
-				: <Text style = { [titleStyle, { fontSize: size / 2, color: "white" } ] }>{ title }</Text>
+			{ source // Show image or title text
+				? <FastImage style = { avatarSize } source = {{ uri: source }} />
+				: <Text style = { [center, avatarSize, { fontSize: size / 2, color: "white" }, titleStyle] }>{ title }</Text>
+			}
+			{ showEdit && <TouchableOpacity
+				onPress = { onPress }
+				style = { [editButton, center, { height: size / 4, width: size / 4, borderRadius: size / 8 }] }
+			>
+				<Icon type = "MaterialIcons" name = "edit" size = { size / 6 } color = "white" />
+			</TouchableOpacity>
 			}
 		</TouchableOpacity>
 	);
@@ -63,6 +74,15 @@ export const Avatar = ({ title, titleStyle, shape = "circle", size = "medium", s
 const styles = {
 	center: {
 		justifyContent: "center",
-		alignItems: "center"
+		alignItems: "center",
+		textAlign: "center",
+		textAlignVertical: "center"
+	},
+	editButton: {
+		backgroundColor: "#21252B",
+		position: "absolute",
+		elevation: 5,
+		bottom: 0,
+		right: 0
 	}
 };
