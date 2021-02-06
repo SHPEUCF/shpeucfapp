dce006102acb48c3c57a21f80277423ca4f0acb5
idelmr <idel_martinez@knights.ucf.edu>
2021-02-05 21:32:06 -0500

    Move all firebase calls to services

--- src/ducks/User.js
+++ src/ducks/User.js
@@ -1,309 +1,45 @@
-import firebase from 'firebase';
 import { createActionTypes } from '@/utils/actions';
-import { Alert } from '@/components';
-import Lodash from 'lodash';
+import { loadCurrentUser } from '@/services/user';
 
 const ACTIONS = createActionTypes([
-	'SHOW_FIREBASE_ERROR',
-	'VERIFIED_USER',
-	'LOGIN_USER',
-	'LOGOUT_USER',
 	'LOAD_USER_AND_PRIVILEGE'
 ]);
 
-const INITIAL_STATE = {
-	activeUser: {
-		firstName: '',
-		color: '#21252b',
-		flag: '',
-		lastName: '',
-		email: '',
-		major: 'Do not wish to disclose',
-		picture: '',
-		points: 0,
-		privilege: {},
-		country: 'Do not wish to disclose',
-		gender: 'Do not wish to disclose',
-		birthday: '0000-00-00',
-		paidMember: false,
-		id: '',
-		voted: false,
-		applied: false,
-		userCommittees: {}
-	},
-	error: '',
-	loading: false
-};
+/**
+ * @typedef {Object} UserState
+ * @prop {string}  id              User's ID assigned by Firebase
+ * @prop {string}  firstName       User's first name
+ * @prop {string}  lastName        User's last name
+ * @prop {string}  email           User's knights email
+ * @prop {string}  flag            User's flag of preference
+ * @prop {string}  picture         User's profile picture
+ * @prop {number}  points          Total points for current semester
+ * @prop {Object}  privilege       User privilege (user, board, e-board)
+ * @prop {Object}  userCommittees  Committees user belongs to
+ * @prop {string}  major           User's major
+ * @prop {string}  country         User's country of origin
+ * @prop {string}  gender          User's gender
+ * @prop {string}  birthday        User's birthday
+ * @prop {boolean} paidMember      Paid member status for current year
+ * @prop {boolean} applied         Applied status for current election
+ * @prop {boolean} voted           Voted status for current election
+ */
 
-export default (state = INITIAL_STATE, action) => {
-	const { payload } = action;
+/**
+ * @type {UserState}
+ */
 
-	switch (action.type) {
-		case ACTIONS.SHOW_FIREBASE_ERROR:
-			return {
-				...state,
-				error: payload
-			};
-		case ACTIONS.LOGIN_USER:
-			return {
-				...state,
-				loading: true
-			};
-		case ACTIONS.LOGOUT_USER:
-			return {
-				...state,
-				...INITIAL_STATE
-			};
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, { payload, type }) => {
+	switch (type) {
 		case ACTIONS.LOAD_USER_AND_PRIVILEGE:
-			return {
-				...state,
-				activeUser: payload
-			};
+			return { ...state, ...payload };
 		default:
 			return state;
 	}
 };
 
-/* Redux Action Creators */
-
-/**
- * Changes the error on the reducer. This information should later be read
- * and displayed in some way to the user.
- *
- * @access     private
- * @param {String}   error An Error Message
- */
-
-const showFirebaseError = (dispatch, error) => {
-	let errorMessage;
-
-	switch (error.code) {
-		case 'auth/user-not-found':
-			errorMessage = 'There is no user record corresponding to this identifier';
-			break;
-		case 'auth/invalid-email':
-			errorMessage = 'Enter a valid email';
-			break;
-		case 'auth/wrong-password':
-			errorMessage = 'Incorrect credentials';
-			break;
-		default:
-			errorMessage = error.message;
-	}
-
-	dispatch({
-		type: ACTIONS.SHOW_FIREBASE_ERROR,
-		payload: errorMessage
-	});
-};
-
-/**
- * Creates a user on Firebase using their knights email as the username with their
- * desired password. Then if succeeded it calls createUserSuccess().
- *
- * Needs to be passed through mapDispatchToProps to ensure error functionality works
- *
- * @access     public
- * @param {Object}   user a user object with all desired user properties
- * @param {String}   email a student knights email
- * @param {String}   password a password that meets the requirements
- */
-
-export const createUser = user => {
-	return (dispatch) => {
-		firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
-			.then(({ user: { uid } }) => createUserSuccess({ ...Lodash.omit(user, 'password'), uid }))
-			.catch((error) => showFirebaseError(dispatch, error));
-	};
-};
-
-/**
- * Creates a user on Firebase Database with the desired fields. Then displays
- * an alert to the user letting them know to check their email.
- *
- *
- * @access     private
- * @param {Object}   user a user object with all desired user properties
- */
-
-const createUserSuccess = (user) => {
-	const {
-		currentUser
-	} = firebase.auth();
-
-	firebase.database().ref(`/users/${currentUser.uid}/`)
-		.set({ ...INITIAL_STATE.activeUser, ...user, id: currentUser.uid })
-		.then(() => {
-			firebase.database().ref(`/points/${currentUser.uid}/`).set({
-				firstName: user.firstName,
-				lastName: user.lastName,
-				points: 0,
-				id: currentUser.uid
-			});
-		})
-		.then(() => {
-			firebase.database().ref(`/privileges/${currentUser.uid}/`).set({
-				firstName: user.firstName,
-				lastName: user.lastName,
-				user: true,
-				board: false,
-				eboard: false,
-				president: false,
-				id: currentUser.uid,
-				paidMember: false
-			});
-		})
-		.then(() => {
-			currentUser.sendEmailVerification()
-				.catch(() => Alert.alert('We were not able to send an email.'
-					+ ' Please contact the Tech Director for assistance'))
-				.then(() => Alert.alert(`We sent a verification to: ${user.email}.
-					Please open your email and verify your account`));
-		})
-		.then(() => firebase.auth().signOut())
-		.catch((error) => Alert.alert(error, { type: 'error' }));
-};
-
-/**
- * Sends a request to firebase to reset the password using the email. Then displays an alert
- * to the user that an email to reset their password was sent to them.
- *
- * Needs to be passed through mapDispatchToProps to ensure error functionality works
- *
- * @access     public
- * @param {String}   email a student knights email
- */
-
-export const resetPassword = (email) => {
-	return (dispatch) => {
-		firebase.auth().sendPasswordResetEmail(email)
-			.then(() => Alert.alert(`Reset Started.\n\
-				If an account with email ${email} exists, a reset password email will be sent. \
-				Please check your email.`))
-			.catch(error => showFirebaseError(dispatch, error));
-	};
-};
-
-/**
- * Sends a request to firebase to login to the account.
- *
- * If the user is not verified then they will be alerted to check their email.
- *
- * @access     public
- * @param {String}   email a student knights email
- * @param {String}   password the user's password
- */
-
-export const loginUser = (email, password) => {
-	return dispatch => {
-		return new Promise((resolve, reject) => {
-			firebase.auth().signInWithEmailAndPassword(email, password)
-				.then(() => {
-					if (!firebase.auth().currentUser.emailVerified) {
-						Alert.alert('Account must be verified!\nPlease check your email for verification email');
-						reject();
-					}
-					else {
-						resolve();
-					}
-				})
-				.catch(error => {
-					showFirebaseError(dispatch, error);
-					reject();
-				});
-		});
-	};
-};
-
-/**
- * Sends a GET request to firebase to load some user's information.
- *
- * This can load the logged in user or if a userID is passed in then it will load
- * that user instead.
- *
- * Needs to be passed through mapDispatchToProps
- *
- * @access     public
- * @param {String}   userID OPTIONAL- the desired user's userID
- */
-
-export const loadUser = (userID) => {
-	const {
-		currentUser
-	} = firebase.auth();
-
-	const id = typeof userID === 'undefined' ? currentUser.uid : userID;
-
-	return (dispatch) => {
-		if (currentUser) {
-			firebase.database().ref(`/users/${id}/`).on('value', userSnapshot => {
-				const user = userSnapshot.val();
-
-				firebase.database().ref(`/privileges/${id}/`).on('value', privilegeSnapshot => {
-					const userWithPrivilege = {
-						...user,
-						privilege: privilegeSnapshot.val()
-					};
-
-					dispatch({
-						type: ACTIONS.LOAD_USER_AND_PRIVILEGE,
-						payload: userWithPrivilege
-					});
-				});
-			});
-		}
-	};
-};
-
-/* FireBase Functions that don't use Redux */
-
-/**
- * Sends an UPDATE request and updates the user on the database.
- *
- * To use you can send all fields of a user that you would like to change.
- * omitting fields does not break the code.
- *
- * ***DO NOT PASS IN THROUGH mapDispatchToProps***
- *
- * @access     public
- * @param {Object}   user        User is an object that can store any subset of props that you would want to edit.
- *
- * **Things you can not edit using this function:**
- * ['firstName', 'lastName', 'points', 'voted', 'privileges', 'paidMember']
- * @example
- * user = {
- * 	nationality: 'Cuban',
- * 	gender: 'Male'
- * }
- *
- * user = {
- * 	picture: 'someUrl.jpg'
- * }
- *
- */
-
-export const editUser = (user) => {
-	const {
-		currentUser
-	} = firebase.auth();
-
-	const invalidProperties = ['firstName', 'lastName', 'points', 'voted', 'privileges', 'paidMember'];
-
-	invalidProperties.forEach(prop => {
-		if (prop in user) console.error(`Please do not try to edit ${prop}!`);
-	});
-
-	firebase.database().ref(`/users/${currentUser.uid}/`).update(user)
-		.catch(error => Alert.alert(error, { type: 'error' }));
-};
-
-/**
- * Sends an signOut request to firebase
- *
- * ***DO NOT PASS IN THROUGH mapDispatchToProps***
- */
-
-export const logoutUser = () => {
-	firebase.auth().signOut()
-		.then(Alert.alert('Signed Out', { title: 'Have a great day!', type: 'success' }));
+export const loadUser = () => dispatch => {
+	loadCurrentUser().then(payload => dispatch({ type: ACTIONS.LOAD_USER_AND_PRIVILEGE, payload }));
 };
---
+++ src/services/user.js
@@ -0,0 +1,165 @@
+import firebase from 'firebase';
+import _ from 'lodash';
+import { Alert } from '@/components';
+
+/**
+ * Sends a GET request to firebase to load the current user's information.
+ */
+
+export const loadCurrentUser = () => new Promise(resolve => {
+	const { currentUser } = firebase.auth();
+
+	firebase.database().ref(`/users/${currentUser.uid}/`).on('value', userSnapshot => {
+		firebase.database().ref(`/privileges/${currentUser.uid}/`).on('value', privilegeSnapshot => {
+			resolve({ ...userSnapshot.val(), privilege: privilegeSnapshot.val() });
+		});
+	});
+});
+
+/**
+ * @typedef {Object} User
+ * @prop {string}  id              User's ID assigned by Firebase
+ * @prop {string}  firstName       User's first name
+ * @prop {string}  lastName        User's last name
+ * @prop {string}  email           User's knights email
+ * @prop {string}  flag            User's flag of preference
+ * @prop {string}  color           User's color of preference
+ * @prop {string}  picture         User's profile picture
+ * @prop {number}  points          Total points for current semester
+ * @prop {Object}  privilege       User privilege (user, board, e-board)
+ * @prop {Object}  userCommittees  Committee(s) user belongs to
+ * @prop {string}  major           User's major
+ * @prop {string}  country         User's country of origin
+ * @prop {string}  gender          User's gender
+ * @prop {string}  birthday        User's birthday
+ * @prop {boolean} paidMember      Paid member status for current year
+ * @prop {boolean} applied         Applied status for current election
+ * @prop {boolean} voted           Voted status for current election
+ */
+
+/**
+ * Creates a user on Firebase using their knights email as the username with their
+ * desired password. If it succeeds, it calls createUserSuccess.
+ *
+ * @param {Pick<User, 'firstName' | 'lastName' | 'email' | 'password' |
+ *                    'gender' | 'major' | 'country' | 'birthday'}       user      User object
+ * @param {Function?}                                                    setError  Callback to update error state on calling component.
+ */
+
+export const createUser = (user, setError) => {
+	firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+		.then(({ user: { uid } }) => createUserSuccess({ ..._.omit(user, 'password'), id: uid }))
+		.catch(error => showFirebaseError(error, setError));
+};
+
+/**
+ * Creates a user on Firebase database with the desired fields. Then displays
+ * an alert to the user letting them know to check their email.
+ *
+ * @param {Object} user User object
+ */
+
+const createUserSuccess = user => {
+	const { firstName, lastName, email, id } = user;
+	const USER_STATUS = {
+		gender: 'Do not wish to disclose',
+		major: 'Do not wish to disclose',
+		country: 'Do not wish to disclose',
+		birthday: '0000-00-00',
+		points: 0,
+		privilege: {},
+		userCommittees: {}
+	};
+
+	firebase.database().ref(`/users/${id}/`).set({ ...USER_STATUS, ...user })
+		.then(() => firebase.database().ref(`/points/${id}/`).set({ firstName, lastName, id, points: 0 }))
+		.then(() => firebase.database().ref(`/privileges/${id}/`).set({ firstName, lastName, id, user: true }))
+		.then(() => firebase.auth().currentUser.sendEmailVerification()
+			.then(() => Alert.alert(`We sent a verification to: ${email}. Please open your email and verify your account`))
+			.catch(() => Alert.alert('We were not able to send an email. Please contact the Tech Director for assistance'))
+		)
+		.catch(error => Alert.alert(error, { type: 'error' }));
+};
+
+/**
+ * Sends an UPDATE request and updates the user on the database.
+ *
+ * @param {Omit<User, 'firstName' | 'lastName' | 'points' | 'voted' | 'privileges' | 'paidMember'>} user User object
+ */
+
+export const editUser = user => {
+	const invalid = ['firstName', 'lastName', 'points', 'voted', 'privileges', 'paidMember'];
+
+	firebase.database().ref(`/users/${user.id}/`).update(_.pickBy(user, value => value && !invalid.includes(value)))
+		.then(() => Alert.alert('Profile edited!', { type: 'success' }))
+		.catch(error => Alert.alert(error.message, { type: 'error' }));
+};
+
+/**
+ * Sends a request to firebase to reset the password using the email. Then displays an alert
+ * to the user that an email to reset their password was sent to them.
+ *
+ * @param {string}    email     User's knights email
+ * @param {Function?} setError  Callback to update error state on calling component.
+ */
+
+export const resetPassword = async (email, setError) => (
+	firebase.auth().sendPasswordResetEmail(email)
+		.then(() => Alert.alert(`If an account with email ${email} exists, \
+a reset password email will be sent. Please check your email.`)
+		)
+		.catch(error => showFirebaseError(error, setError))
+);
+
+/**
+ * Sends a request to firebase to login to the account.
+ * If the user is not verified then they will be alerted to check their email.
+ *
+ * @param {string}    email     User's knights email
+ * @param {string}    password  User's password
+ * @param {Function?} setError  Callback to update error state on calling component.
+ */
+
+export const loginUser = (email, password, setError) => {
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(() => {
+			if (!firebase.auth().currentUser.emailVerified)
+				Alert.alert('Account must be verified!\nPlease check your email for the verification email');
+		})
+		.catch(error => showFirebaseError(error, setError));
+};
+
+/**
+ * Sends an signOut request to firebase.
+ */
+
+export const logoutUser = () => {
+	firebase.auth().signOut()
+		.then(Alert.alert('Signed Out', { title: 'Have a great day!', type: 'success' }));
+};
+
+/**
+ * Updates error state in calling component.
+ *
+ * @param {Object}    error
+ * @param {string}    error.code     Firebase error code
+ * @param {string}    error.message  Firebase error message
+ * @param {Function?} setError       Function to update error state
+ */
+
+const showFirebaseError = ({ code, message }, setError) => {
+	setError && setError((() => {
+		switch (code) {
+			case 'auth/user-not-found':
+				return 'There is no user record corresponding to this identifier';
+			case 'auth/invalid-email':
+				return 'Enter a valid email';
+			case 'auth/wrong-password':
+				return 'Incorrect credentials';
+			default:
+				return message;
+		}
+	})());
+
+	return code;
+};
