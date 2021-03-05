d0d2377ef6b4c6b52e975919a0808d36f99535f1
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2021-03-04 19:08:38 -0500

    replaced env file with config

---
+++ config.js
@@ -0,0 +1,25 @@
+// Testing Databases
+module.exports = {
+	// SHPE UCF Database
+	shpeDB: {
+		apiKey: "AIzaSyCeX5lUZUmQxXsWNO8gNXVHqfJs-kQmSaY",
+		authDomain:"shpe-ucf.firebaseapp.com",
+		databaseURL:"https://shpe-ucf.firebaseio.com",
+		projectId:"shpe-ucf",
+		storageBucket:"shpe-ucf.appspot.com",
+		messagingSenderId:"974032317047",
+		appId:"1:974032317047:web:0a4a2ad01ac705b90ff472",
+		version:"1.12"
+	},
+	// SHPE APP TEST Database
+	testDB: {
+		apiKey:"AIzaSyBjuggUaZwZEj0FYMK3x-O7FflhdsOUsx0",
+		authDomain:"shpe-app-test.firebaseapp.com",
+		databaseURL:"https://shpe-app-test.firebaseio.com",
+		projectId:"shpe-app-test",
+		storageBucket:"shpe-app-test.appspot.com",
+		messagingSenderId:"343434443699",
+		appId:"1:343434443699:web:430ce49ccf542aec2fe9c6",
+		version:"1.12"
+	}
+};
