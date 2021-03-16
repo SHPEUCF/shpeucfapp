22f04638767acbbfb8c48551321db00a10e4040c
idelmr <idel_martinez@knights.ucf.edu>
2021-03-16 00:37:51 -0400

    Add animation type and update docs

--- src/components/general/Modal.js
+++ src/components/general/Modal.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { TouchableOpacity, BackHandler, Dimensions, SafeAreaView, ViewStyle } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { TouchableOpacity, BackHandler, Dimensions, View, ViewStyle } from 'react-native';
+import { Animated } from './';
 
 const { height, width } = Dimensions.get('screen');
 
@@ -28,13 +29,14 @@ const BackButton = ({ onHide }) => {
  * Displays a dialog on top of the current screen.
  *
  * @typedef {Object} ModalProps
- * @prop {boolean}    visible            Control modal visibility
- * @prop {Function}   onHide             Callback to hide modal on backdrop or back button
- * @prop {number=}    priority           Controls zIndex of modal, larger priority modals show up on top
- * @prop {boolean=}   dismissBackdrop    Dismiss modal by tapping the backdrop
- * @prop {boolean=}   dismissBackButton  Dismiss modal with Android back button
- * @prop {ViewStyle=} backdrop           Modal backdrop style
- * @prop {ViewStyle}  style              Modal content style
+ * @prop {boolean}          visible            Control modal visibility
+ * @prop {Function}         onHide             Callback to hide modal on backdrop or back button
+ * @prop {number=}          priority           Controls zIndex of modal, larger priority modals show up on top
+ * @prop {boolean=}         dismissBackdrop    Dismiss modal by tapping the backdrop
+ * @prop {boolean=}         dismissBackButton  Dismiss modal with Android back button
+ * @prop {'slide' | 'fade'} animation          Animation type for Modal
+ * @prop {ViewStyle=}       backdrop           Modal backdrop style
+ * @prop {ViewStyle}        style              Modal content style
  *
  * @param {ModalProps}
  */
@@ -46,25 +48,61 @@ export const Modal = ({
 	dismissBackdrop = true,
 	dismissBackButton = true,
 	backdrop = styles.backdrop,
+	animation = 'slide',
 	style,
 	children
 }) => {
+	const [innerVisible, setInnerVisible] = useState(false);
 	const { modal, childCenter } = styles;
+	const content = useRef();
+
+	useEffect(() => {
+		if (visible)
+			setInnerVisible(true);
+
+		if (content.current) {
+			switch (animation) {
+				case 'fade':
+					content.current.animation('timing', { toValue: visible ? 0 : -60, duration: 900 })
+						.start(() => !visible && setInnerVisible(false));
+					break;
+				case 'slide':
+					content.current.animation('timing', { toValue: visible ? 0 : height, duration: 825, easing: visible ? ['out', 'back'] : 'back' })
+						.start(() => !visible && setInnerVisible(false));
+			}
+		}
+	}, [visible]);
 
 	return (
-		visible &&
-		<SafeAreaView style = { [modal, { elevation: priority + 7, zIndex: priority + 99 }] }>
+		(visible || innerVisible) &&
+		<View style = { [modal, { elevation: priority + 99, zIndex: priority + 99 }] }>
 			<TouchableOpacity
 				activeOpacity = { 1 }
 				onPress = { dismissBackdrop && onHide }
 				style = { [{ height, width, justifyContent: 'center' }, backdrop] }
 			>
-				<TouchableOpacity activeOpacity = { 1 } style = { [childCenter, style] }>
-					{ children }
-				</TouchableOpacity>
+				{ (animation === 'fade')
+					? <Animated.View
+						property = 'marginTop'
+						initial = { -60 }
+						interpolate = {{ property: 'opacity', ranges: { inputRange: [-60, 0], outputRange: [0, 1] } }}
+						style = { [childCenter, style] }
+						ref = { content }
+					>
+						{ children }
+					</Animated.View>
+					: <Animated.View
+						property = 'marginTop'
+						initial = { height }
+						style = { [childCenter, style] }
+						ref = { content }
+					>
+						{ children }
+					</Animated.View>
+				}
 			</TouchableOpacity>
 			{ visible && <BackButton onHide = { dismissBackButton && onHide } /> }
-		</SafeAreaView>
+		</View>
 	);
 };
 
