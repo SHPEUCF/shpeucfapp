2e8db49151395389830ce7fb5f6779abd6751a65
idelmr <idel_martinez@knights.ucf.edu>
2021-03-24 18:44:09 -0400

    Revert "Fix image upload and remove rn-fetch-blob"
    This reverts commit 40b79676069eb68bab75e4fd1f3a46ce74f0526e.

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import firebase from 'firebase';
+import { Icon } from '@/components';
 import ImagePicker from 'react-native-image-crop-picker';
-import { Icon, Alert } from '@/components';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export const stockImg = 'https://cdn0.iconfinder.com/data/icons/superuser-web-kit/512/686909-user_people_man_human_head_person-512.png';
 
@@ -34,39 +35,59 @@ export const truncateNames = item => {
 	return [firstName, lastName];
 };
 
-const uriToBlob = uri => new Promise((resolve, reject) => {
-	const xhr = new XMLHttpRequest();
-
-	xhr.onload = () => resolve(xhr.response); // Resolves with blob
-	xhr.onerror = () => reject('Could not convert to Blob');
-	xhr.responseType = 'blob'; // Ensure a blob is returned
-
-	xhr.open('GET', uri, true);
-	xhr.send(null);
-});
-
 export const openGallery = (filePath, fileName, onImageStoreFunction) => {
+	const Blob = RNFetchBlob.polyfill.Blob;
+	const fs = RNFetchBlob.fs;
+	const storeImageUrl = (url, filePath) => {
+		firebase.database().ref(`${filePath}/`).update({
+			picture: url
+		});
+	};
+
+	window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+	window.Blob = Blob;
+
 	ImagePicker.openPicker({
 		width: 300,
 		height: 400,
+		includeBase64: true,
 		compressImageQuality: 0.8,
 		mediaType: 'photo',
 		cropping: true,
 		cropperCircleOverlay: true
 	}).then(image => {
 		if (!image.filename) image.filename = image.path.split('/').pop();
-
-		const storeImageUrl = (url, filePath) => firebase.database().ref(`${filePath}/`).update({ picture: url });
-		const imageRef = firebase.storage().ref(filePath).child(fileName || image.filename);
+		const imagePath = image.path;
 		let uploadBlob = null;
+		let mime = 'image/jpg';
+		/*
+		 * Normally pictures are uploaded under a consistent filename so that pictures are automatically overwritten
+		 * If there is no consistent filename passed in, this will allow for infite uploads to the same filepath
+		 */
+		const imageRef = firebase.storage().ref(filePath).child(fileName || image.filename);
 
-		uriToBlob(image.path)
-			.then(blob => imageRef.put(uploadBlob = blob, { contentType: 'image/jpg' }))
-			.then(() => imageRef.getDownloadURL())
-			.then(url => onImageStoreFunction ? onImageStoreFunction(url, filePath) : storeImageUrl(url, filePath))
-			.then(() => Alert.alert('Successfully updated profile picture', { type: 'success' }))
-			.catch(() => Alert.alert('Failed updating profile picture', { type: 'error' }))
-			.finally(() => uploadBlob && uploadBlob.close());
+		fs.readFile(imagePath, 'base64')
+			.then((data) => {
+				// console.log(data);
+				return Blob.build(data, { type: `${mime};BASE64` });
+			})
+			.then((blob) => {
+				uploadBlob = blob;
+
+				return imageRef.put(blob, { contentType: mime });
+			})
+			.then(() => {
+				uploadBlob.close();
+
+				return imageRef.getDownloadURL();
+			})
+			.then((url) => {
+				// checks for custom function and if there is no custom function then do default
+				onImageStoreFunction ? onImageStoreFunction(url, filePath) : storeImageUrl(url, filePath);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	});
 };
 
