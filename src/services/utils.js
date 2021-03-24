23761199b352561d8393da5ff7befa54ea2fb102
idelmr <idel_martinez@knights.ucf.edu>
2021-03-24 18:57:44 -0400

    Fix editUser function for single-child objects and create deepCloneWithout function

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 /**
  * Updates error state in calling component.
  *
@@ -17,4 +19,24 @@ export const showFirebaseError = ({ code, message }) => {
 		default:
 			return message;
 	}
+};
+
+/**
+ * Clones object recursively, omitting `invalid` keys.
+ * Sets objects with a single null child to false.
+ *
+ * @param {Object}   obj      Object to be cloned
+ * @param {string[]} invalid  Array of possible invalid keys inside `obj`
+ */
+
+export const cloneDeepWithout = (obj, invalid) => {
+	const innerClone = deepObj => _.transform(deepObj, (accumulator, value, key) => {
+		if (invalid.includes(key)) return;
+
+		accumulator[key] = _.isObject(value)
+			?	_.every(value, value => value === null) ? false : innerClone(value)
+			: value;
+	});
+
+	return innerClone(obj);
 };
