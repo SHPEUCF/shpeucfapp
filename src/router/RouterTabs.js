af1b32086b9dacd4e29022a1fc364297f8973691
idelmr <idel_martinez@knights.ucf.edu>
2021-03-29 13:32:27 -0400

    Add mask to tab bar icons and text

--- src/router/RouterTabs.js
+++ src/router/RouterTabs.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
+import MaskedView from '@react-native-community/masked-view';
 import { Icon } from '@/components';
 
 export const RouterTabs = ({ state, navigation }) => {
 	const Icons = ['newspaper', 'ios-calendar', 'ios-person', 'ios-menu'];
-	const { tabRow, tab } = styles;
+	const { tabRow, mask } = styles;
 
 	const Tab = ({ route, index }) => {
 		const isFocused = state.index === index;
@@ -16,12 +17,18 @@ export const RouterTabs = ({ state, navigation }) => {
 		};
 
 		return (
-			<TouchableOpacity onPress = { onPress } style = { tab }>
-				<Icon name = { Icons[index] } size = { 30 } style = {{ color: isFocused ? '#FFC107' : 'white' }} />
-				<Text style = {{ color: isFocused ? '#E0E6ED' : '#C0CCDA' }}>
-					{ route.name }
-				</Text>
-			</TouchableOpacity>
+			<MaskedView
+				onStartShouldSetResponder = { () => true }
+				onResponderGrant = { onPress }
+				style = {{ flex: 1 }}
+				maskElement = { <View style = { mask }>
+					<Icon name = { Icons[index] } size = { 30 } />
+					<Text>{ route.name }</Text>
+				</View> }
+			>
+				{ isFocused && <View style = {{ position: 'absolute', zIndex: 1, transform: [{ translateX: 45 }], height: '100%', width: '100%', backgroundColor: '#FFC107' }} /> }
+				<View style = {{ flex: 1, backgroundColor: 'white' }} />
+			</MaskedView>
 		);
 	};
 
@@ -39,9 +46,10 @@ const styles = {
 		backgroundColor: '#21252b',
 		justifyContent: 'space-around'
 	},
-	tab: {
+	mask: {
+		backgroundColor: 'transparent',
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'space-around'
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 };
